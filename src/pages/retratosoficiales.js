bf64ee290ee27f882ad6e5be071b94a05c10ec66
acuati <benalibenaliabdelkbir@gmail.com>
2024-09-24 13:30:36 +0200

    actualizo cambios y espero que ya definitivos

--- src/pages/retratosoficiales.js
+++ src/pages/retratosoficiales.js
@@ -1,10 +1,9 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Card from "../components/card" // Importar el nuevo componente de tarjeta
+
 
 
 
